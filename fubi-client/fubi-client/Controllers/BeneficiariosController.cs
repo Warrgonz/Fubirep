9a20cd26d4531d1a9af1b04b9a753e89df880a2c
JDRC20 <jrojas50929@ufide.ac.cr>
2024-11-22 16:45:10 -0600

    update beneficiarios

--- fubi-client/fubi-client/Controllers/BeneficiariosController.cs
+++ fubi-client/fubi-client/Controllers/BeneficiariosController.cs
@@ -15,118 +15,166 @@ namespace fubi_client.Controllers
             _conf = conf;
         }
 
-        // Mostrar todos los beneficiarios
+        // Acción para listar los beneficiarios
         public async Task<IActionResult> Index()
         {
             using (var client = _http.CreateClient())
             {
-                var url = _conf.GetSection("Variables:UrlApi").Value + "Beneficiarios/GetAllBeneficiarios";
+                var url = _conf.GetSection("Variables:UrlApi").Value + "Beneficiarios/GetAll";
                 var response = await client.GetAsync(url);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    var result = await response.Content.ReadFromJsonAsync<List<Beneficiarios>>();
-                    return View(result);
+                    var beneficiarios = await response.Content.ReadFromJsonAsync<List<Beneficiarios>>();
+                    return View(beneficiarios);
                 }
                 else
                 {
-                    ViewBag.ErrorMessage = "Hubo un error al cargar los beneficiarios";
+                    ViewBag.ErrorMessage = "No se pudo cargar la lista de beneficiarios.";
                     return View(new List<Beneficiarios>());
                 }
             }
         }
 
-        // Crear un nuevo beneficiario (GET)
+        // Acción para mostrar el formulario de creación
         [HttpGet]
         public IActionResult CreateBeneficiario()
         {
             return View();
         }
 
-        // Crear un nuevo beneficiario (POST)
+        // Acción para crear un beneficiario
         [HttpPost]
-        public async Task<IActionResult> CreateBeneficiario(Beneficiarios model)
+        public async Task<IActionResult> CreateBeneficiario(Beneficiarios model, IFormFile ruta_imagen)
         {
             using (var client = _http.CreateClient())
             {
-                var url = _conf.GetSection("Variables:UrlApi").Value + "Beneficiarios/CreateBeneficiario";
+                // 1. Crear beneficiario sin imagen
+                var url = _conf.GetSection("Variables:UrlApi").Value + "Beneficiarios/Create";
                 var beneficiarioContent = JsonContent.Create(model);
 
                 var response = await client.PostAsync(url, beneficiarioContent);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("Index");
+                    // 2. Subir imagen
+                    if (ruta_imagen != null && ruta_imagen.Length > 0)
+                    {
+                        var imageUrl = $"{_conf.GetSection("Variables:UrlApi").Value}Beneficiarios/UploadImage/{model.Id}";
+                        var formContent = new MultipartFormDataContent();
+                        formContent.Add(new StreamContent(ruta_imagen.OpenReadStream()), "file", ruta_imagen.FileName);
+
+                        var uploadResponse = await client.PostAsync(imageUrl, formContent);
+
+                        if (uploadResponse.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Index", "Beneficiarios");
+                        }
+                        else
+                        {
+                            ViewBag.ErrorMessage = "No se pudo cargar la imagen.";
+                            return View(model);
+                        }
+                    }
+
+                    return RedirectToAction("Index", "Beneficiarios");
                 }
                 else
                 {
                     var result = await response.Content.ReadFromJsonAsync<Respuesta>();
-                    ViewBag.ErrorMessage = result?.Codigo == -2 ? result.Mensaje : "Hubo un error interno";
+
+                    if (result.Codigo == -2)
+                    {
+                        ViewBag.ErrorMessage = result.Mensaje;
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Hubo un error interno";
+                    }
                     return View(model);
                 }
             }
         }
 
-        // Editar un beneficiario (GET)
+        // Acción para mostrar el formulario de edición
         [HttpGet]
         public async Task<IActionResult> EditBeneficiario(int id)
         {
             using (var client = _http.CreateClient())
             {
-                var url = _conf.GetSection("Variables:UrlApi").Value + $"Beneficiarios/GetBeneficiario/{id}";
+                var url = $"{_conf.GetSection("Variables:UrlApi").Value}Beneficiarios/GetById/{id}";
                 var response = await client.GetAsync(url);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    var result = await response.Content.ReadFromJsonAsync<Beneficiarios>();
-                    return View(result);
+                    var beneficiario = await response.Content.ReadFromJsonAsync<Beneficiarios>();
+                    return View(beneficiario);
                 }
                 else
                 {
-                    ViewBag.ErrorMessage = "Hubo un error al cargar los datos del beneficiario";
+                    ViewBag.ErrorMessage = "No se pudo cargar el beneficiario.";
                     return RedirectToAction("Index");
                 }
             }
         }
 
-        // Editar un beneficiario (POST)
+        // Acción para actualizar un beneficiario
         [HttpPost]
-        public async Task<IActionResult> EditBeneficiario(Beneficiarios model)
+        public async Task<IActionResult> EditBeneficiario(Beneficiarios model, IFormFile ruta_imagen)
         {
             using (var client = _http.CreateClient())
             {
-                var url = _conf.GetSection("Variables:UrlApi").Value + "Beneficiarios/UpdateBeneficiario";
+                // 1. Actualizar beneficiario
+                var url = _conf.GetSection("Variables:UrlApi").Value + "Beneficiarios/Update";
                 var beneficiarioContent = JsonContent.Create(model);
 
                 var response = await client.PutAsync(url, beneficiarioContent);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("Index");
+                    // 2. Subir nueva imagen si se proporcionó
+                    if (ruta_imagen != null && ruta_imagen.Length > 0)
+                    {
+                        var imageUrl = $"{_conf.GetSection("Variables:UrlApi").Value}Beneficiarios/UploadImage/{model.Id}";
+                        var formContent = new MultipartFormDataContent();
+                        formContent.Add(new StreamContent(ruta_imagen.OpenReadStream()), "file", ruta_imagen.FileName);
+
+                        var uploadResponse = await client.PostAsync(imageUrl, formContent);
+
+                        if (!uploadResponse.IsSuccessStatusCode)
+                        {
+                            ViewBag.ErrorMessage = "No se pudo actualizar la imagen.";
+                            return View(model);
+                        }
+                    }
+
+                    return RedirectToAction("Index", "Beneficiarios");
                 }
                 else
                 {
-                    ViewBag.ErrorMessage = "Hubo un error al actualizar el beneficiario";
+                    var result = await response.Content.ReadFromJsonAsync<Respuesta>();
+                    ViewBag.ErrorMessage = result?.Mensaje ?? "Hubo un error interno.";
                     return View(model);
                 }
             }
         }
 
-        // Eliminar un beneficiario
+        // Acción para eliminar un beneficiario
+        [HttpPost]
         public async Task<IActionResult> DeleteBeneficiario(int id)
         {
             using (var client = _http.CreateClient())
             {
-                var url = _conf.GetSection("Variables:UrlApi").Value + $"Beneficiarios/DeleteBeneficiario/{id}";
+                var url = $"{_conf.GetSection("Variables:UrlApi").Value}Beneficiarios/Delete/{id}";
                 var response = await client.DeleteAsync(url);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    return RedirectToAction("Index");
+                    return RedirectToAction("Index", "Beneficiarios");
                 }
                 else
                 {
-                    ViewBag.ErrorMessage = "Hubo un error al eliminar el beneficiario";
+                    ViewBag.ErrorMessage = "No se pudo eliminar el beneficiario.";
                     return RedirectToAction("Index");
                 }
             }
